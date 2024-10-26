5d66cec9dc2e30f47344d1f41e2dba394ab99ea6
nikita <erm.nikita1212@gmail.com>
2024-10-26 19:56:03 +0300

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,71 +1,102 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
 import "./Footer.css";
-
+import logofooter from "../../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTelegram } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faTiktok } from "@fortawesome/free-brands-svg-icons";
 const Footer = () => {
-  const { t } = useTranslation();
-
   return (
-    <footer>
-      <div className="footer-container">
-        <div className="footer-column">
-          <h3 className="footer-heading">{t("footer_company")}</h3>
-          <ul className="footer-links">
-            <li>
-              <a href="#about">{t("footer_about")}</a>
-            </li>
-            <li>
-              <a href="#careers">{t("footer_careers")}</a>
-            </li>
-            <li>
-              <a href="#contact">{t("footer_contact")}</a>
-            </li>
-          </ul>
+    <footer className="footer">
+      <div className="footer-img-descr-social">
+        <div className="footer-logo">
+          <img src={logofooter} alt="" className="img-footer" />
+          <h1>Student's Job</h1>
         </div>
-        <div className="footer-column">
-          <h3 className="footer-heading">{t("footer_support")}</h3>
-          <ul className="footer-links">
-            <li>
-              <a href="#faq">{t("footer_faq")}</a>
-            </li>
-            <li>
-              <a href="#help-center">{t("footer_help_center")}</a>
-            </li>
-            <li>
-              <a href="#returns">{t("footer_returns")}</a>
-            </li>
-          </ul>
+        <div className="footer-description">
+          <p>
+            We help people, especially those who find it difficult to find a
+            job, giving everyone the opportunity to fulfill their dreams!
+          </p>
         </div>
-        <div className="footer-column">
-          <h3 className="footer-heading">{t("footer_account")}</h3>
-          <ul className="footer-links">
-            <li>
-              <a href="#login">{t("footer_login")}</a>
-            </li>
-            <li>
-              <a href="#sign-up">{t("footer_sign_up")}</a>
-            </li>
-            <li>
-              <a href="#logout">{t("footer_logout")}</a>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-column">
-          <h3 className="footer-heading">{t("footer_connect")}</h3>
-          <ul className="footer-links">
-            <li>
-              <a href="#instagram">{t("footer_instagram")}</a>
-            </li>
-            <li>
-              <a href="#telegram">{t("footer_telegram")}</a>
-            </li>
-            <li>
-              <a href="#vk">{t("footer_vk")}</a>
-            </li>
-          </ul>
+        <div className="footer-social">
+          <a href="https://www.instagram.com" aria-label="Instagram">
+            <FontAwesomeIcon
+              icon={faInstagram}
+              style={{
+                color: "#acacac",
+                width: "27px",
+                height: "27px",
+                transition: "transform 0.3s ease, color 0.3s ease",
+              }}
+              onMouseEnter={(e) => (e.currentTarget.style.color = "#ffffff")}
+              onMouseLeave={(e) => (e.currentTarget.style.color = "#acacac")}
+            />
+          </a>
+
+          <a href="https://www.instagram.com" aria-label="Telegram">
+            <FontAwesomeIcon
+              icon={faTelegram}
+              style={{
+                color: "#acacac",
+                width: "27px",
+                height: "27px",
+                transition: "transform 0.3s ease, color 0.3s ease",
+              }}
+              onMouseEnter={(e) => (e.currentTarget.style.color = "#ffffff")}
+              onMouseLeave={(e) => (e.currentTarget.style.color = "#acacac")}
+            />
+          </a>
+          <a href="https://www.instagram.com" aria-label="Github">
+            <FontAwesomeIcon
+              icon={faGithub}
+              style={{
+                color: "#acacac",
+                width: "27px",
+                height: "27px",
+                transition: "transform 0.3s ease, color 0.3s ease",
+              }}
+              onMouseEnter={(e) => (e.currentTarget.style.color = "#ffffff")}
+              onMouseLeave={(e) => (e.currentTarget.style.color = "#acacac")}
+            />
+          </a>
+          <FontAwesomeIcon
+            icon={faTiktok}
+            style={{
+              color: "#acacac",
+              width: "27px",
+              height: "27px",
+              transition: "transform 0.3s ease, color 0.3s ease",
+            }}
+            onMouseEnter={(e) => (e.currentTarget.style.color = "#ffffff")}
+            onMouseLeave={(e) => (e.currentTarget.style.color = "#acacac")}
+          />
         </div>
       </div>
-      <p className="footer-copy">{t("footer_copy")}</p>
+      <div className="footer-pages">
+        <div className="heading-footer-pages">
+          <h1>Pages</h1>
+        </div>
+        <div className="paragraph-footer-pages">
+          <p>Vacancies</p>
+          <p>Summary</p>
+          <p>Chat</p>
+          <p>About</p>
+          <p>Auction</p>
+        </div>
+      </div>
+      <div className="footer-additional">
+        <div className="heading-footer-additional">
+          <h1>Additional</h1>
+        </div>
+        <div className="paragraph-footer-additional">
+          <p>Our foundation</p>
+          <p>Our Mission</p>
+          <p>Who we are</p>
+          <p>FAQ</p>
+        </div>
+      </div>
     </footer>
   );
 };
