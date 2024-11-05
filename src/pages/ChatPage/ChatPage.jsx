734897d25d96d89561d638c170428f424f36866f
nikita <erm.nikita1212@gmail.com>
2024-11-05 11:09:00 +0300

    commit

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -127,7 +127,7 @@ const ChatPage = () => {
         const { company, vacancy, selectedFileName, coverLetter } =
           JSON.parse(applicationData);
 
-        const lastMessage = vacancy; //
+        const lastMessage = vacancy;
 
         if (
           !chats.find(
