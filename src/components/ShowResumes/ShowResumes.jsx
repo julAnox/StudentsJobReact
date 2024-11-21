0bcf7c8c96d549ab24ef2143aafd72775ad5374c
nikita <erm.nikita1212@gmail.com>
2024-11-21 15:06:01 +0300

    commit

--- src/components/ShowResumes/ShowResumes.jsx
+++ src/components/ShowResumes/ShowResumes.jsx
@@ -38,7 +38,6 @@ function ShowResumes() {
     <div className="resumes-dashboard-container">
       <div className="resumes-dashboard-card">
         <h2 className="resumes-dashboard-title">Your Resumes</h2>
-
         <div className="resumes-dashboard-list">
           {resumes.length > 0 ? (
             resumes.map((resume, index) => (
